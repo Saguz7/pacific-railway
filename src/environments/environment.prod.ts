904e2fd815a76ccd3857f3fbbcd1ac76da2987cf
Omar Vasquez <ovasquez.al@gmail.com>
2023-05-02 14:13:17 -0600

    variables  dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APP_URL_BASE: 'https://saguz7.github.io/pacific-railway/',
+  APP_URL_BASE: 'https://dev.d2ucmupqh49t6j.amplifyapp.com/',
   API_URL_BASE: 'https://zt1nm5f67j.execute-api.us-west-2.amazonaws.com/dev/',
 
 };
