455caa38956d46b62a7ac7aa1de2e6a0b6c9c4b2
saguz7 <cesarsantiagoguzman@gmail.com>
2023-07-24 11:38:30 -0600

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,19 @@
+import { Environment } from './environment';
+import { readFileSync } from 'fs';
 
-export const environment = {
+const envFile = readFileSync('.env.production', 'utf-8');
+const envLines = envFile.split('\n');
+
+const environment: Environment = {
   production: true,
-  API_URL_BASE: process.env.API_URL_BASE,
-  REGION_POOL: process.env.REGION_POOL,
-  USER_POOL_ID: process.env.USER_POOL_ID,
-  APP_POOL_ID: process.env.APP_POOL_ID,
-  DOMAIN_SIGN: process.env.DOMAIN_SIGN,
-  REDIRECT_SIGNIN: process.env.REDIRECT_SIGNIN,
-  REDIRECT_SIGNOUT: process.env.REDIRECT_SIGNOUT,
-  IDENTITY_PROVIDER: process.env.IDENTITY_PROVIDER
+  API_URL_BASE: envLines.find(line => line.startsWith('API_URL_BASE='))?.split('=')[1] || 'https://vuycilier4.execute-api.us-west-2.amazonaws.com/dev/',
+  REGION_POOL: envLines.find(line => line.startsWith('REGION_POOL='))?.split('=')[1] || 'us-west-2',
+  USER_POOL_ID: envLines.find(line => line.startsWith('USER_POOL_ID='))?.split('=')[1] || 'us-west-2_YKTiEMjtU',
+  APP_POOL_ID: envLines.find(line => line.startsWith('APP_POOL_ID='))?.split('=')[1] || 's7ch645u8voh00dridmn8kn19',
+  DOMAIN_SIGN: envLines.find(line => line.startsWith('DOMAIN_SIGN='))?.split('=')[1] || 'cpkc-chassis-management-dev.auth.us-west-2.amazoncognito.com',
+  REDIRECT_SIGNIN: envLines.find(line => line.startsWith('REDIRECT_SIGNIN='))?.split('=')[1] || 'https://dev.d1klk34joigd80.amplifyapp.com/current-chassis-location',
+  REDIRECT_SIGNOUT: envLines.find(line => line.startsWith('REDIRECT_SIGNOUT='))?.split('=')[1] || 'https://dev.d1klk34joigd80.amplifyapp.com/current-chassis-location',
+  IDENTITY_PROVIDER: envLines.find(line => line.startsWith('IDENTITY_PROVIDER='))?.split('=')[1] || 'CognitoF5',
 };
+
+export { environment };
